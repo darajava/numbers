72190538bda5d72ac1de5e309a9c480a6d94b713
Dara Javaherian <darajavaherian@gmail.com>
2020-03-26 04:27:22 +0000

    add harder numbers

--- src/App.svelte
+++ src/App.svelte
@@ -24,11 +24,11 @@
   }
 
   const generateNewNumbers = () => {
-    // return [
-    //   shuffle(
-    //     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-    //   ).slice(0, 7), Math.round(Math.random() * 1000),
-    // ];
+    return [
+      shuffle(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+      ).slice(0, 7), Math.round(Math.random() * 1000),
+    ];
     return [shuffle([1, 2, 3, 4, 5, 6, 7]).slice(0, 7), Math.round(Math.random() * 7)];
   }
 
