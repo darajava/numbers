bea14accd0623ffc9b3cc6842041848d97bf5bb2
Dara Javaherian <darajavaherian@gmail.com>
2020-03-27 00:34:17 +0000

    Add Hint.svelte

---
+++ src/Hint.svelte
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+  export let hint = '';  
+</script>
+
+<div class="hint">
+  {hint}
+</div>
+
+<style>
+  .hint {
+    font-size: 10px;
+    font-family: courier, monospace;
+    color: white;
+    position: absolute;
+    bottom: 0px;
+    right: 3px;
+    opacity: 0.5;
+  }
+</style>
